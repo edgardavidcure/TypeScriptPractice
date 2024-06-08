5768cec915ca6b237a971a3829a41f3b1a103422
edgar <edgardavid1503@gmail.com>
2024-06-07 18:38:50 -0700

    first commit

---
+++ tailwind.config.ts
@@ -0,0 +1,43 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+
+  ],
+  theme: {
+    theme: {
+  
+      colors: {
+        'light-orange': "#e6c887",
+        'extra-light-orange': "#fcf9f0",
+  
+        'dark-blue': "#143642",
+  
+        'light-gray': "#f6f7f9",
+        
+        'white': 'hsl(0, 0%, 100%)',
+  
+        'black': 'hsl(0, 0%, 0%)',
+  
+        'transparent': 'rgba(0, 0, 0, 0)',
+        
+        'yellow': '#ffc43d',
+  
+        'green': '#11B136',
+  
+        'gray': '#959595',
+  
+        'red': '#fb3640',
+  
+        'cgreen': '#b4c424',
+        'corange': '#f78c3b',
+        'cblue': '#00bbda',
+        'cpink': '#ed5672'
+      }
+    },
+  },
+  plugins: [],
+};
